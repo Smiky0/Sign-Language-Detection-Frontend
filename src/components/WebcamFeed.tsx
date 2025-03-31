0743c666a2a91b0547b2e52cc7e38209430f694b
Soumik <smik1172@gmail.com>
2025-03-31 16:46:58 +0530

    feat: connecting to backend

--- src/components/WebcamFeed.tsx
+++ src/components/WebcamFeed.tsx
@@ -1,39 +1,122 @@
-import { useState, useCallback, FC, JSX } from "react";
+import { useState, useCallback, FC, JSX, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
 
 interface WebcamFeed {
     showVideo: boolean;
+    pauseDetected: boolean;
     updateVideo: () => void;
+    handDetection: (data: boolean) => void;
+    updatePrediction: (data: string) => void;
+    socketUrl: string;
 }
 
 const WebcamFeed: FC<WebcamFeed> = ({
     showVideo,
+    pauseDetected,
     updateVideo,
+    handDetection,
+    updatePrediction,
+    socketUrl,
 }): JSX.Element => {
     const [isWebcamAvailable, setIsWebcamAvailable] = useState(true);
+
+    const [showSocketStatus, setSocketStatus] = useState<string>(
+        "Press start video to begin"
+    );
+    const webcamRef = useRef<Webcam>(null);
+    const socketRef = useRef<WebSocket | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(
+        document.createElement("canvas")
+    );
     const handleUserMediaError = useCallback(() => {
         setIsWebcamAvailable(false);
         updateVideo();
     }, []);
 
+    const captureFrame = useCallback(() => {
+        const video = webcamRef.current?.video;
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        if (video && ctx) {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            canvas.toBlob(
+                (blob) => {
+                    if (
+                        blob &&
+                        socketRef.current?.readyState === WebSocket.OPEN
+                    ) {
+                        blob.arrayBuffer().then((buffer) => {
+                            socketRef.current?.send(buffer);
+                        });
+                    }
+                },
+                "image/webp",
+                0.8
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!showVideo || pauseDetected) return;
+
+        socketRef.current = new WebSocket(socketUrl);
+        socketRef.current.onopen = () => {
+            setSocketStatus("Hand detection started 👏");
+        };
+
+        // get predicted data from server
+        socketRef.current.onmessage = (event) => {
+            console.log("Received text message:", event.data);
+            updatePrediction(event.data);
+        };
+
+        socketRef.current.onerror = (error) => {
+            console.error("WebSocket error:", error);
+        };
+        socketRef.current.onclose = () => {
+            setSocketStatus("Hand detection stopped 👎");
+        };
+
+        const intervalId = setInterval(captureFrame, 100);
+        WebSocket.OPEN ? handDetection(false) : handDetection(true);
+
+        return () => {
+            clearInterval(intervalId);
+            socketRef.current?.close();
+        };
+    }, [socketUrl, captureFrame, showVideo, handDetection]);
+
     return (
         <>
             {isWebcamAvailable && showVideo ? (
                 <>
-                    <img
-                        src="http://localhost:5000/video_feed"
-                        onLoad={() => setIsWebcamAvailable(true)}
+                    <Webcam
+                        audio={false}
+                        height={720}
+                        width={1280}
+                        mirrored={true}
                         onError={handleUserMediaError}
-                        alt=""
-                        className="w-full aspect-video rounded-2xl border-2 border-white/40"
+                        ref={webcamRef}
+                        disablePictureInPicture={true}
+                        screenshotFormat="image/jpeg"
+                        className="w-full aspect-video bg-background/40 border-1 rounded-2xl"
                     />
                 </>
             ) : (
-                <div className="w-full aspect-video h-auto p-4 flex justify-center items-center rounded-2xl border-2 border-white/40">
-                    <p className="text-2xl tracking-wide">
-                        Error showing video
+                <div className="w-full bg-background/40 border-1 aspect-video flex justify-center items-center rounded-2xl">
+                    <p className="capitalize text-md sm:text-2xl tracking-wide">
+                        no video feed available
                     </p>
                 </div>
             )}
+            <p className="text-lg sm:text-xl tracking-wide mt-4 w-full rounded-2xl py-2 px-4">
+                <span className="capitalize font-semibold">status: </span>{" "}
+                {showSocketStatus}
+            </p>
         </>
     );
 };
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,49 +2,97 @@ import { useState } from "react";
 import WebcamFeed from "../components/WebcamFeed";
 import Button from "../components/Button";
 import { FaVideo, FaVideoSlash } from "react-icons/fa6";
+import { MdClearAll } from "react-icons/md";
+import { HiBackspace } from "react-icons/hi";
+import { TiTick } from "react-icons/ti";
+import Toggle from "../components/Toogle";
 
 export default function Homepage() {
-    const [showDetectedText, setDetectedText] = useState<string>(
-        "Once you start, your text will appear here..."
-    );
-    const [showVideo, setShowVideo] = useState<boolean>(true);
+    const [showDetectedText, setDetectedText] = useState<string>("");
+    const [showVideo, setShowVideo] = useState<boolean>(false);
+    const [pauseDetected, setPauseDetection] = useState<boolean>(false);
+    const handleHandDetection = (data: boolean) => {
+        setPauseDetection(data);
+    };
     const handleShowVideo = () => {
         setShowVideo(!showVideo);
     };
+    const handleDetectedWord = (predictedText: string) => {
+        setDetectedText((prevText) => prevText + predictedText);
+    };
+    const handleBackspace = () => {
+        setDetectedText((prevText) => prevText.slice(0, -1));
+    };
+
+    const clearPredictedWords = () => {
+        setDetectedText("");
+    };
+
     return (
         <>
-            <div className="flex justify-center gap-8 mx-8">
-                <div className="flex-3 w-full aspect-video text-align-center bg-transparent rounded-2xl">
-                    <div className="flex justify-between items-center rounded-2xl p-3 my-4 bg-gradient-to-r backdrop-blur-sm from-blue-300/60 to bg-purple-400/60">
-                        <p className="text-xl font-medium capitalize">
-                            Sign Language Detection
-                        </p>
+            <div className="flex flex-col lg:flex-row justify-center gap-0 md:gap-8 px-4 sm:px-10">
+                <div className="flex-3 mb-4 w-full bg-white/90 border-1 p-2 sm:p-4 rounded-2xl">
+                    <div className="capitalize flex flex-col sm:flex-row justify-between items-center rounded-2xl mb-2 sm:mb-4 bg-transparent">
+                        <Toggle
+                            label="Hand prediction"
+                            onChange={() => setPauseDetection(!pauseDetected)}
+                            checked={!pauseDetected}
+                            className="my-2"
+                        />
                         <Button
-                            label={showVideo ? "Stop Video" : "Start Video"}
+                            label={showVideo ? "Stop video" : "Start video"}
+                            variant="solid"
                             icon={showVideo ? <FaVideoSlash /> : <FaVideo />}
                             onClick={handleShowVideo}
                             style={{
                                 backgroundColor: showVideo
-                                    ? "#eb4034"
-                                    : "white",
-                                color: showVideo ? "white" : "black",
+                                    ? "#ff2c2c"
+                                    : "black",
                             }}
                         />
                     </div>
+
                     <WebcamFeed
                         showVideo={showVideo}
                         updateVideo={handleShowVideo}
+                        handDetection={handleHandDetection}
+                        updatePrediction={handleDetectedWord}
+                        pauseDetected={pauseDetected}
+                        socketUrl={"ws://127.0.0.1/predict"}
                     />
-                    <div className="flex gap-8 justify-center items-center pt-8"></div>
                 </div>
-                <div className="flex-1 w-md h-full flex flex-col justify-center items-center rounded-2xl p-3 my-4 backdrop-blur-sm bg-gradient-to-r from-blue-300/40 to-purple-400/40">
-                    <p className="text-xl text-center capitalize font-semibold pb-2">
-                        Translated text
-                    </p>
-                    <p className="text-md tracking-wider text-wrap p-2 rounded-2xl bg-amber-50 border-2 border-white/40">
-                        {showDetectedText}
-                    </p>
-                    <Button label="Auto Correct" className="my-2" />
+                <div className="flex-1 mb-4 w-full flex flex-col justify-between items-center bg-white/90 border-1 p-2 sm:p-4 rounded-2xl">
+                    <div className="w-full">
+                        <p className="text-md sm:text-2xl text-center capitalize font-semibold py-4">
+                            Translated text
+                        </p>
+
+                        <p className="text-sm sm:text-lg tracking-wider text-wrap mt-2 p-2 rounded-2xl bg-background/40 border-1 h-30 md:h-100 overflow-y-auto">
+                            {showDetectedText}
+                        </p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center m-2 gap-2">
+                        <Button
+                            label="auto Correct all"
+                            variant="solid"
+                            icon={<TiTick />}
+                        />
+                        <div className="flex gap-2">
+                            <Button
+                                label="backspace"
+                                variant="outline"
+                                icon={<HiBackspace />}
+                                onClick={handleBackspace}
+                            />
+
+                            <Button
+                                label="Clear"
+                                variant="outline"
+                                icon={<MdClearAll />}
+                                onClick={clearPredictedWords}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
