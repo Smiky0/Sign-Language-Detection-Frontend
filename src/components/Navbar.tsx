7e31ee22926a6d407d57040834c1367b2c2dfde4
Soumik <smik1172@gmail.com>
2025-04-02 08:42:21 +0530

    remove: logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
     return (
         <div className="flex items-center justify-start p-2 sm:p-6">
-            <img
+            {/* <img
                 src="/logo.png"
                 width={50}
                 height={50}
                 className="m-2 sm:mr-6"
-            />
+            /> */}
             <span className="capitalize w-1/2 text-xl sm:text-3xl tracking-tight cursor-pointer font-semibold text-balance">
                 Sign Language Detection
             </span>
